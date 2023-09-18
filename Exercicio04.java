b947cc4e4befe075d93c8d6e40d9f5af795ec862
Leandro Rosa <137298525+LDRRosa@users.noreply.github.com>
2023-09-18 08:04:46 -0300

    Add files via upload

---
+++ Exercicio04.java
@@ -0,0 +1,43 @@
+package lista2Prog2;
+
+import java.util.Scanner;
+
+public class Exercicio04TEMQUEACABR {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] matriz = new int[63][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("Digite os numeros para preencher a matriz: ");
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+
+        for (int i = 0; i < calculohistograma(matriz).length; i++) {
+            System.out.printf("%d: %d%n",  i, calculohistograma(matriz)[i]);
+        }
+        scanner.close();
+    }
+
+    public static int[] calculohistograma(int matriz[][]) {
+        int valormaximo = 63;
+        int[] histograma = new int[3 + 1];
+        int valorMaisFrequente = -1;
+        int maxFrequencia = 0;
+
+        for (int i = 0; i < valormaximo; i++) {
+            for (int j = 0; j < valormaximo; j++) {
+                int valor = matriz[i][j];
+                if (valor >= 0 && valor <= valormaximo) {
+                    histograma[valor]++;
+                    if (histograma[valor] > maxFrequencia) {
+                        maxFrequencia = histograma[valor];
+                        valorMaisFrequente = valor;
+                    }
+                }
+            }
+        }
+        return histograma;
+
+    }
+}
