71a759c9d7dc63b83b19e8097ca90c863b2865a7
Leandro Rosa <137298525+LDRRosa@users.noreply.github.com>
2023-09-19 15:27:12 -0300

    Exercicio04.java

--- Exercicio04.java
+++ Exercicio04.java
@@ -1,43 +1,68 @@
+/* Faça um método que receba uma matriz de inteiros (variando de 0 a 63), a quantidade de linhas e
+colunas na matriz e calcule e imprima o histograma da matriz retornando o valor que mais apareceu
+(o histograma é o diagrama de frequência e informa quantas vezes cada valor, dentro do intervalo
+válido, ocorreu na matriz) */
 package lista2Prog2;
-//Tenho que terminar(não funciona!!)
+
+import java.util.Random;
 import java.util.Scanner;
 
-public class Exercicio04TEMQUEACABR {
+public class Exercicio04 {
+
+    public static int linhas, colunas;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[][] matriz = new int[63][3];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.println("Digite os numeros para preencher a matriz: ");
-                matriz[i][j] = scanner.nextInt();
+        Random random = new Random();
+
+        System.out.println("Digite o número de linhas da matriz: ");
+        linhas = scanner.nextInt();
+        System.out.println("Digite o número de colunas da matriz: ");
+        colunas = scanner.nextInt();
+
+        
+        int[][] matriz = new int[linhas][colunas];
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                // System.out.println("digite os números para preencer a matriz: ");
+                matriz[i][j] = random.nextInt(50);
             }
         }
-
-        for (int i = 0; i < calculohistograma(matriz).length; i++) {
-            System.out.printf("%d: %d%n",  i, calculohistograma(matriz)[i]);
+        System.out.println();
+        System.out.println("matriz inserida:");
+        for (int i = 0; i < linhas; i++) {
+            System.out.println();
+            for (int j = 0; j < colunas; j++) {
+                System.out.printf("%3d",matriz[i][j]);
+            }
         }
         scanner.close();
+        System.out.println("\n================================================================");
+        System.out.println("histograma:");
+        calculaHistograma(matriz, linhas, colunas);
     }
 
-    public static int[] calculohistograma(int matriz[][]) {
-        int valormaximo = 63;
-        int[] histograma = new int[3 + 1];
-        int valorMaisFrequente = -1;
-        int maxFrequencia = 0;
+    public static void calculaHistograma(int[][] matriz, int linhas, int colunas) {
+        int valmax = 63; // O intervalo válido é de 0 a 63
+        int[] histograma = new int[valmax + 1];
+        int valmais = -1;
+        int maxfrequencia = 0;
 
-        for (int i = 0; i < valormaximo; i++) {
-            for (int j = 0; j < valormaximo; j++) {
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
                 int valor = matriz[i][j];
-                if (valor >= 0 && valor <= valormaximo) {
+                if (valor >= 0 && valor <= valmax) {
                     histograma[valor]++;
-                    if (histograma[valor] > maxFrequencia) {
-                        maxFrequencia = histograma[valor];
-                        valorMaisFrequente = valor;
+                    if (histograma[valor] > maxfrequencia) {
+                        maxfrequencia = histograma[valor];
+                        valmais = valor;
                     }
                 }
             }
         }
-        return histograma;
-
+        for (int i = 0; i < 63; i++) {
+            System.out.printf("%d: %d%n", i, histograma[i]);
+        }
+        System.out.println("Valor mais frequente: "+valmais);
     }
 }
