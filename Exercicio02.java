665de2494e2b0df29799ffc2f596aba222eaa643
Leandro Rosa <137298525+LDRRosa@users.noreply.github.com>
2023-09-19 15:26:39 -0300

    Exercicio02.java

--- Exercicio02.java
+++ Exercicio02.java
@@ -1,68 +1,117 @@
-/* Faça um método que receba uma matriz de inteiros (variando de 0 a 63), a quantidade de linhas e
-colunas na matriz e calcule e imprima o histograma da matriz retornando o valor que mais apareceu
-(o histograma é o diagrama de frequência e informa quantas vezes cada valor, dentro do intervalo
-válido, ocorreu na matriz) */
 package lista2Prog2;
 
 import java.util.Random;
 import java.util.Scanner;
 
-public class Exercicio04 {
+public class Exercicio02 {
 
     public static int linhas, colunas;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+        int[][] matriz = new int[20][20];
+        int i, j;
 
-        System.out.println("Digite o número de linhas da matriz: ");
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 4; j++) {
+                // System.out.println("Digite os valores para preencher a matriz:");
+                //matriz[i][j] = scanner.nextint;
+                matriz[i][j] = random.nextInt(10);
+            }
+        }
+        System.out.println("Digite quantas linhas da matriz voce quer processar :");
         linhas = scanner.nextInt();
-        System.out.println("Digite o número de colunas da matriz: ");
+        System.out.println("Digite quantas colunas da matriz voce quer processar:");
         colunas = scanner.nextInt();
-
-        
-        int[][] matriz = new int[linhas][colunas];
-        for (int i = 0; i < linhas; i++) {
-            for (int j = 0; j < colunas; j++) {
-                // System.out.println("digite os números para preencer a matriz: ");
-                matriz[i][j] = random.nextInt(50);
-            }
-        }
+        System.out.println("O maior valor no intervalo é: " + retornamaior(matriz));
+        System.out.println("O menor valor no intervalo é: " + retornamenor(matriz));
+        System.out.println("A média dos valores no intervalo é: " + retornamedia(matriz));
+        escrevematriz(matriz);
         System.out.println();
-        System.out.println("matriz inserida:");
-        for (int i = 0; i < linhas; i++) {
-            System.out.println();
-            for (int j = 0; j < colunas; j++) {
-                System.out.printf("%3d",matriz[i][j]);
-            }
-        }
+        escrevediagonal(matriz);
+        System.out.println();
+        System.out.println("desvio padrao: " + retornadesvio(matriz));
         scanner.close();
-        System.out.println("\n================================================================");
-        System.out.println("histograma:");
-        calculaHistograma(matriz, linhas, colunas);
     }
 
-    public static void calculaHistograma(int[][] matriz, int linhas, int colunas) {
-        int valmax = 63; // O intervalo válido é de 0 a 63
-        int[] histograma = new int[valmax + 1];
-        int valmais = -1;
-        int maxfrequencia = 0;
+    public static int retornamaior(int maior[][]) {
 
+        int maiorvalor = 0;
         for (int i = 0; i < linhas; i++) {
             for (int j = 0; j < colunas; j++) {
-                int valor = matriz[i][j];
-                if (valor >= 0 && valor <= valmax) {
-                    histograma[valor]++;
-                    if (histograma[valor] > maxfrequencia) {
-                        maxfrequencia = histograma[valor];
-                        valmais = valor;
-                    }
+                if (maior[i][j] > maiorvalor) {
+                    maiorvalor = maior[i][j];
                 }
             }
         }
-        for (int i = 0; i < 63; i++) {
-            System.out.printf("%d: %d%n", i, histograma[i]);
+        return maiorvalor;
+
+    }
+
+    public static int retornamenor(int menor[][]) {
+
+        int menorvalor = Integer.MAX_VALUE, i = 0, j = 0;
+        for (i = 0; i < linhas; i++) {
+            for (j = 0; j < colunas; j++) {
+                if (menor[i][j] < menorvalor) {
+                    menorvalor = menor[i][j];
+                }
+            }
         }
-        System.out.println("Valor mais frequente: "+valmais);
+        return menorvalor;
+    }
+
+    public static double retornamedia(int media[][]) {
+
+        double mediatotal = 0;
+        int soma = 0, i = 0, j = 0;
+        for (i = 0; i < linhas; i++) {
+            for (j = 0; j < colunas; j++) {
+                soma += media[i][j];
+            }
+        }
+        mediatotal = soma / (linhas * colunas);
+        return mediatotal;
+    }
+
+    public static void escrevematriz(int matriz[][]) {
+        System.out.println("matiz digitada: ");
+        for (int i = 0; i < linhas; i++) {
+            System.out.print("\n");
+            for (int j = 0; j < colunas; j++) {
+                System.out.printf("%3d", matriz[i][j]);
+            }
+
+        }
+    }
+
+    public static void escrevediagonal(int matriz[][]) {
+        if (colunas == linhas) {
+            System.out.println("Diagonal principal:");
+            for (int i = 0; i < colunas; i++) {
+                System.out.print(matriz[i][i] + " ");
+            }
+        } else {
+            System.out.println("não exixte diagonal principal.0");
+        }
+
+    }
+
+    public static double retornadesvio(int des[][]) {
+        double desviopadrao = 0;
+        double somaQuadradosDiferencas = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                double diferenca = des[i][j] - retornamedia(des);
+                somaQuadradosDiferencas += diferenca * diferenca;
+            }
+        }
+        desviopadrao = Math.sqrt(somaQuadradosDiferencas / 16);
+        return desviopadrao;
+    }
+
+    public static void normalizamatriz(int matriz[][]) {
+        
     }
 }
